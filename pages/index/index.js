e370530613c10e72b01357e975bef59f3de7be3a
kongzheng1993 <kongzheng1993@hotmail.com>
2019-02-16 19:09:13 +0800

    完善轮播图&增加产品信息展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,19 +11,62 @@ Page({
       "url": "images/2.jpg"
     }, {
       "url": "images/3.jpg"
+    }],
+    recommends: [{
+      "id": 1,
+      "name": "recommendName1",
+      "content": "recommendContent1"
+    }, {
+      "id": 2,
+      "name": "recommendName2",
+      "content": "recommendContent2"
+    }, {
+      "id": 3,
+      "name": "recommendName3",
+      "content": "recommendContent3"
+    }],
+    infoTypes: [{
+      "id": "1",
+      "name": "infoType1"
+    }, {
+      "id": "2",
+      "name": "infoType2"
+    }, {
+      "id": "3",
+      "name": "infoType3"
+    }, {
+      "id": "4",
+      "name": "infoType4"
+    }],
+    infoList: [{
+      "id": "1",
+      "name": "name1",
+      "content": "content1"
+    }, {
+      "id": "2",
+      "name": "name2",
+      "content": "content2"
+    }, {
+      "id": "3",
+      "name": "name3",
+      "content": "content3"
+    }, {
+      "id": "4",
+      "name": "name4",
+      "content": "content4"
     }]
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     wx.authorize({
       scope: 'scope.userLocation',
       success(res) {
@@ -49,50 +92,72 @@ Page({
           }
         })
       },
-      fail() { },
-      complete() { }
+      fail() {},
+      complete() {}
     })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+
+  //滑动切换 
+  swiperTab: function(e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  //点击切换 
+  clickTab: function(e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   }
+
+
 })
