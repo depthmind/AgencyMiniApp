2c9bfe61a94bee55d613ec4fa92a95a8cd00d63b
liuhan <lh@tourmade.com>
2019-02-26 12:02:08 +0800

    代理商入驻

--- pages/agencySignin/agencySignin.js
+++ pages/agencySignin/agencySignin.js
@@ -13,13 +13,24 @@ Page({
     addressDetail: '',
     logoImagePath: '/images/logo-image.jpg',
     wechatImagePath: '/images/logo-image.jpg',
+    licence1ImagePath: '/images/logo-image.jpg',
+    licence2ImagePath: '/images/logo-image.jpg',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    wx.request({ //查询可选择的入驻时长参数
+      url: 'http://localhost:8080/Agency/parameter/findParameter.do?paraDomain=agency.validPeriod',
+      success(res) {
+        console.log(res)
+        that.setData({
+          validPeriod: res.data
+        })
+      }
+    })
   },
 
   /**
@@ -124,6 +135,38 @@ Page({
     })
   },
 
+  chooseLicence1Image: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        that.setData({
+          licence1ImagePath: tempFilePaths
+        })
+      }
+    })
+  },
+
+  chooseLicence2Image: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        that.setData({
+          licence2ImagePath: tempFilePaths
+        })
+      }
+    })
+  },
+
   chooseAddress: function () {
     wx.navigateTo({
       url: '/pages/map/map',
@@ -140,7 +183,7 @@ Page({
     var data = e.detail.value
     var validateCode= data.validateCode //先判断验证码是否正确
     var parameter = "agencyName=" + data.agencyName + "&addressDetail=" + data.addressDetail + "&serviceNumber=" + data.serviceNumber + "&mobilephone=" + data.mobilephone + "&time=" + data.time
-    wx.uploadFile({
+    wx.uploadFile({ // 上传代理商logo
       url: 'http://47.105.169.49/Agency/upfile',
       filePath: that.data.logoImagePath[0],
       name: 'file',
@@ -150,7 +193,7 @@ Page({
       success(res) {
         var path = res.data
         parameter = parameter + '&logoImagePath=' + path
-        wx.uploadFile({
+        wx.uploadFile({ // 上传老板微信
           url: 'http://47.105.169.49/Agency/upfile',
           filePath: that.data.wechatImagePath[0],
           name: 'file',
@@ -160,8 +203,35 @@ Page({
           success(res) {
             var path = res.data
             parameter = parameter + '&wechatImagePath=' + path
-            wx.request({
-              url: 'http://localhost:8080/Agency/agency/saveAgencyBase.do?' + parameter,
+            wx.uploadFile({ // 上传营业执照
+              url: 'http://47.105.169.49/Agency/upfile',
+              filePath: that.data.wechatImagePath[0],
+              name: 'file',
+              formData: {
+                user: 'test'
+              },
+              success(res) {
+                var path = res.data
+                parameter = parameter + '&licence1ImagePath=' + path
+                wx.uploadFile({ // 上传食品经营许可证
+                  url: 'http://47.105.169.49/Agency/upfile',
+                  filePath: that.data.wechatImagePath[0],
+                  name: 'file',
+                  formData: {
+                    user: 'test'
+                  },
+                  success(res) {
+                    var path = res.data
+                    parameter = parameter + '&licence2ImagePath=' + path
+                    wx.request({
+                      url: 'http://localhost:8080/Agency/agency/saveAgencyBase.do?' + parameter,
+                      success(res) {
+                        
+                      }
+                    })
+                  }
+                })
+              }
             })
           }
         })
