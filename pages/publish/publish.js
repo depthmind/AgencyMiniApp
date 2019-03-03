9b4449095368ef734943b9b3ece9fcbdc729874a
depthmind <depthminds@gmail.com>
2019-03-04 00:54:27 +0800

    发布

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,6 +1,11 @@
-// pages/publish/publish.js
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.js')
+var qqmapsdk = new QQMapWX({
+  key: 'EBNBZ-ELC64-536UJ-XGRBP-FTFGK-OZBMF' // 必填
+})
+var longitude = wx.getStorageSync("longitude")
+var latitude = wx.getStorageSync("latitude")
+var openId = wx.getStorageSync('openId')
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -15,13 +20,58 @@ Page({
     addressDetail: '',
     tempFilePaths: [],
     uploadImagePath: '',
+    publishCategory:'',
+    choosedCategory: '',
+    needPay: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    wx.request({ //查询可选择的类目参数
+      url: 'http://localhost:8080/Agency/parameter/findParameter.do?paraDomain=publish.category',
+      success(res) {
+        console.log(res)
+        that.setData({
+          publishCategorys: res.data
+        })
+      }
+    })
+    wx.request({ //查询如果需要支付时的费用
+      url: 'http://localhost:8080/Agency/parameter/findParameter.do?paraDomain=publish.cost',
+      success(res) {
+        console.log(res)
+        that.setData({
+          publishCost: res.data[0].value
+        })
+      }
+    })
+    //that.translateAddress(latitude, longitude);
+    wx.getLocation({
+      success: function (res) {
+        var new_latitude = res.latitude
+        var new_longitude = res.longitude
+        wx.setStorageSync('latitude', new_latitude)
+        wx.setStorageSync('longitude', new_longitude)
+        that.translateAddress(new_latitude, new_longitude)
+        that.setData({
+          latitude: latitude,
+          longitude: longitude
+        })
+      },
+    })
+    wx.request({ //判断是否已入驻
+      url: 'http://localhost:8080/Agency/agency/findAgencyByOpenId.do?openId=' + openId,
+      success(res) {
+        if (res.data.isCooperation == '1') {
+          that.setData({
+            needPay: false
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -90,7 +140,9 @@ Page({
     console.log("选择产品分类")
     console.log(e)
     this.setData({
-      index: e.detail.value
+      index: e.detail.value,
+      publishCategory: "sss",
+      choosedCategory: e.detail.value
     })
   },
 
@@ -102,6 +154,31 @@ Page({
       sourceType: ['album'],
       success(res) {
         // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        that.uploadDIY(tempFilePaths, 0, 0, 0, tempFilePaths.length)
+        // for (var j = 0; j < tempFilePaths.length; j++) {
+        //   wx.uploadFile({
+        //     url: 'http://47.105.169.49/Agency/upfile',
+        //     filePath: tempFilePaths[j],
+        //     name: 'fileData',
+        //     success: (resp) => {
+        //       console.log(resp.data)
+        //       wx.setStorageSync(tempFilePaths[j], resp.data)
+        //     },
+        //     fail: (res) => {
+
+        //     }
+        //   })
+        // }
+
+        for (var i = 0; i < res.tempFilePaths.length; i++) {
+          for (var j = 0; j < that.data.tempFilePaths.length; j ++) {
+            if (res.tempFilePaths[i] == that.data.tempFilePaths[j]) {
+              res.tempFilePaths[i].splice(i,1)
+            }
+          }
+        }
+        console.log("sss", res.tempFilePaths)
         that.data.tempFilePaths = res.tempFilePaths
         that.setData({
           tempFilePaths: that.data.tempFilePaths
@@ -115,8 +192,29 @@ Page({
   },
 
   chooseAddress: function () {
-    wx.navigateTo({
-      url: '/pages/map/map',
+    // wx.navigateTo({
+    //   url: '/pages/map/map',
+    // })
+    var that = this
+    // wx.getLocation({
+    //   success: function(res) {
+    //     var new_latitude = res.latitude
+    //     var new_longitude = res.longitude
+    //     wx.setStorageSync('latitude', new_latitude)
+    //     wx.setStorageSync('longitude', new_longitude)
+    //     that.translateAddress(new_latitude, new_longitude)
+    //     that.setData({
+    //       latitude: latitude,
+    //       longitude: longitude
+    //     })
+    //   },
+    // })
+    wx.chooseLocation({
+      success: function(res) {
+        that.setData({ //设置markers属性和地图位置poi，将结果在地图展示
+          addressDetail: res.address
+        });
+      },
     })
   },
 
@@ -126,48 +224,175 @@ Page({
     var temp = that.data.tempFilePaths
     var data = e.detail.value
     var isTop = 0
-    if (data.switch) {
-      isTop = 1
-    }
-    var parameter = 'address=' + data.address + '&contactName=' + data.contactName + '&mobilePhone=' + data.mobilePhone
-      + '&description=' + data.description + '&isTop=' + isTop
+    var description = data.description
+    var contactName = data.contactName
+    var mobilephone = data.mobilephone
+    var address = data.address
+    // if (that.data.publishCategory == '') {
+    //   that.showModal("请选择类目")
+    //   return;
+    // }
+    // if (description == undefined || description == '') {
+    //   that.showModal("请填写信息内容")
+    //   return;
+    // }
+    // if (contactName == undefined || contactName == '') {
+    //   that.showModal("请填写联系人姓名")
+    //   return;
+    // }
+    // if (mobilephone == undefined || mobilephone == '') {
+    //   that.showModal("请填写联系人电话")
+    //   return;
+    // }
+    // if (!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(mobilephone))) {
+    //   that.showModal("请填写正确的手机号")
+    //   return;
+    // }
+    // if (address == undefined || address == '') {
+    //   that.showModal("请填写详细地址")
+    //   return;
+    // }
+    // if (data.switch) {
+    //   isTop = 1
+    // }
+    var parameter = 'address=' + data.address + '&contactName=' + data.contactName + '&mobilephone=' + data.mobilephone
+      + '&description=' + data.description + '&category=' + that.data.choosedCategory
     if (temp.length > 0) {
+      var imagePath = ''
       for (var i = 0; i < temp.length; i++) {
-        wx.uploadFile({
-          url: 'http://47.105.169.49/Agency/upfile',
-          filePath: temp[i],
-          name: 'file',
-          formData: {
-            user: 'test'
-          },
-          success(res) {
-            var path = res.data
-            if (that.data.uploadImagePath == '') {
-              that.data.uploadImagePath = path
-            } else {
-              that.data.uploadImagePath = that.data.uploadImagePath + '|' + path 
-            }
-            if (i = temp.length - 1) {
-              console.log(that.data.uploadImagePath)
-              parameter = parameter + '&images=' + that.data.uploadImagePath
+        //that.uploadDIY(temp, 0, 0, 0, temp.length, parameter)
+        if (wx.getStorageSync(temp[i])) {
+          console.log('key', wx.getStorageSync(temp[i]))
+          if (imagePath == '') {
+            imagePath = wx.getStorageSync(temp[i])
+          } else {
+            imagePath = imagePath + '|' + wx.getStorageSync(temp[i])
+          }
+        }
+      }
+      parameter = parameter + '&images=' + encodeURIComponent(imagePath)
+      console.log(parameter)
+    } //直接调用保存发布信息的接口
+    console.log("parameter", parameter)
+    if (that.data.needPay) {
+      wx.request({
+        url: 'http://localhost:8080/Agency/pay/jsapiPay?tradeNo=' + data.mobilephone + '&totalFee=' + that.data.publishCost,
+        success(res) {
+          wx.requestPayment({
+            timeStamp: res.data.timeStamp,
+            nonceStr: res.data.nonceStr,
+            package: res.data.prepayId,
+            signType: 'MD5',
+            paySign: res.data.paySign,
+            success(res) {
               wx.request({
-                url: 'http://47.105.169.49/Agency/publish/savePublishContent.do?' + parameter,
+                url: 'http://localhost:8080/Agency/publish/savePublishContent.do?' + parameter,
+                header: {
+                  'content-type': 'application/json' // 默认值
+                },
                 success(res) {
-                  console.log(res)
+                  that.setData({
+                    uploadImagePath: ''
+                  })
                 }
               })
+              wx.redirectTo({
+                url: '/pages/paySuccess/paySuccess',
+              })
+              console.log(res)
+            },
+            fail(res) {
+              console.log(res)
             }
-          }
-        })
-      }
-    } else { //直接调用保存发布信息的接口
-      wx.request({
-        url: 'http://47.105.169.49/Agency/publish/savePublishContent.do?' + parameter,
-        success(res) {
-          console.log(res)
+          })
         }
       })
+    } else {
+      wx.request({
+        url: 'http://localhost:8080/Agency/publish/savePublishContent.do?' + parameter,
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success(res) {
+          that.setData({
+            uploadImagePath: ''
+          })
+        }
+      })
+      //并且需要跳转到提交成功页面
     }
-    console.log(e)
-  }
+    //提交成功之后需要删除缓存里的图片路径
+  },
+
+  showModal: function (msg) {
+    wx.showModal({
+      title: '提示',
+      content: msg,
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+
+  translateAddress: function (latitude, longitude) {
+    var that = this
+    qqmapsdk.reverseGeocoder({
+      location: latitude + ',' + longitude,
+      success: function (res) {//成功后的回调
+        console.log(res);
+        var address = res.result.address;
+        that.setData({ //设置markers属性和地图位置poi，将结果在地图展示
+          addressDetail: address
+        });
+      },
+      fail: function (error) {
+        console.error(error);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    })
+  },
+  
+  uploadDIY(filePaths, successUp, failUp, i, length) {
+    var that = this
+    wx.uploadFile({
+      url: 'http://47.105.169.49/Agency/upfile',
+      filePath: filePaths[i],
+      name: 'fileData',
+      // formData: {
+      //   'pictureUid': owerId,
+      //   'pictureAid': albumId
+      // },
+      success: (resp) => {
+        console.log(resp)
+        if (wx.getStorageSync(filePaths[i]) != undefined) {
+          wx.setStorageSync(filePaths[i], resp.data)
+        }
+        var tempUploadImagePath = that.data.uploadImagePath
+        if (tempUploadImagePath == '') {
+          that.data.uploadImagePath = resp.data
+        } else {
+          that.data.uploadImagePath = tempUploadImagePath + '|' + resp.data
+        }
+        successUp++;
+      },
+      fail: (res) => {
+        failUp++;
+      },
+      complete: () => {
+        i++;
+        if (i == length) {
+          //this.showToast('总共' + successUp + '张上传成功,' + failUp + '张上传失败！');
+        }
+        else {  //递归调用uploadDIY函数
+          this.uploadDIY(filePaths, successUp, failUp, i, length);
+        }
+      },
+    });
+  },
 })
