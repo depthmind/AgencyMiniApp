d8671e40d0934c1531e3fa5bb3e64f35991b6932
kongzheng1993 <kongzheng1993@hotmail.com>
2019-03-09 00:42:14 +0800

    kongz 查询publish加入tab类型

--- pages/index/index.js
+++ pages/index/index.js
@@ -181,7 +181,8 @@ Page({
       })
     } else {
       this.setData({
-        tabType: 2
+        tabType: 2,
+        
       })
     }
     console.log("tabType=" + this.data.tabType)
@@ -327,6 +328,7 @@ Page({
         offset: that.data.publishOffset,
         rows: that.data.rows,
         province: that.data.currentProvince,
+        paraId: that.data.currentTab,
         city: that.data.currentCity,
         area: that.data.currentArea
       },
@@ -340,7 +342,6 @@ Page({
             icon: "none"
           })
         } else {
-          console.log(publish[2].images)
           for (var i = 0; i < publish.length; i++) {
             if (publish[i].images != undefined) {
               publish[i].images = publish[i].images.split(",")
