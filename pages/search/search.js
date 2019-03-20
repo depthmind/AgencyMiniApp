c1043a7f7a3dd2593dd9f6cbacd3a52c88568065
liuhan <lh@tourmade.com>
2019-03-20 17:51:09 +0800

    优化

--- pages/search/search.js
+++ pages/search/search.js
@@ -92,8 +92,8 @@ Page({
     }
     if (cursor != that.data.inputCursor && value.length > 0) {
       wx.request({
-        //url: 'https://www.caoxianyoushun.com:8443/Agency/agency/findAgencyByName.do',
-        url: 'http://localhost:8080/Agency/agency/findAgencyByName.do',
+        url: 'https://www.caoxianyoushun.com:8443/Agency/agency/findAgencyByName.do',
+        //url: 'http://localhost:8080/Agency/agency/findAgencyByName.do',
         data: {
           name: value,
           offset: 0,
@@ -108,8 +108,8 @@ Page({
         }
       })
       wx.request({
-        //url: 'https://www.caoxianyoushun.com:8443/Agency/goods/getGoods.do',
-        url: 'http://localhost:8080/Agency/goods/getGoods.do',
+        url: 'https://www.caoxianyoushun.com:8443/Agency/goods/getGoods.do',
+        //url: 'http://localhost:8080/Agency/goods/getGoods.do',
         data: {
           goodsName: value,
           offset: 0,
